5bbb9598d1b855c1b44be9d1b344261d38adcda1
MyrasYa <rasyaakbar2310@gmail.com>
2025-12-24 11:38:41 +0700

    Menambahkan fitur validate form

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@
 // MANIPULATE FOR TANGGAL LAHIR
 // ################################
 
-
-
 const Kalender = document.getElementById('Kalender');
 
 const Tanggal = document.getElementById('inputTanggal');
@@ -22,7 +20,7 @@ Kalender.addEventListener('change', function(e) {
             const bulan = fullOutput[1];
             const tahun = fullOutput[0];
             
-            console.log(fullOutput)
+            // console.log(fullOutput)
     
             Tanggal.value = tanggal;
             Bulan.value = bulan;
@@ -35,8 +33,80 @@ Kalender.addEventListener('change', function(e) {
     
 });
 
+// ################################
+// MANIPULATE FOR TANGGAL LAHIR
+// ################################
+
+
+
+
+
 
 
 // ################################
-// MANIPULATE FOR TANGGAL LAHIR
+// MANIPULATE FORM SUBMIT
+// ################################
+
+const form = document.getElementById('valid');
+const btnOK = document.getElementById('submitBtn')
+const validOK = document.getElementById('validOK');
+
+btnOK.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const inputs = document.querySelectorAll(
+        '#namaDepan, #namaBelakang, #email, #tel, #alamatJalan, #Kota, #Provinsi, #Pos, #inputTanggal, #inputBulan, #inputTahun'
+    );
+    const rule = document.getElementById('rule');
+    const policy = document.getElementById('policy');
+
+    let isValid = true;
+
+    inputs.forEach(input => {
+        if (!input.value) {
+            isValid = false;
+            input.style.border = '2px solid red';
+        }
+    })
+
+    if (!rule.checked || !policy.checked) {
+        isValid = false;
+        rule.style.outline = '2px solid red';
+        policy.style.outline = '2px solid red';
+    }
+
+    if (!isValid) {
+        form.innerHTML = 
+        '<h1>Formulir belum lengkap</h1><p>Silakan lengkapi semua bidang yang diperlukan dan setujui syarat & ketentuan serta kebijakan privasi.</p><button id="validOK">Ok</button>';
+        
+        const validOK = document.getElementById('validOK'); 
+        validOK.addEventListener('click', function(e) {
+            form.style.display = 'none';
+            form.style.opacity = '0';
+            form.style.pointerEvents = 'none';
+            form.style.transition = 'all 0.3s ease-in-out';
+        });
+    }
+
+    console.log('ok clicked');
+    form.style.display = 'flex';
+    form.style.opacity = '1';
+    form.style.pointerEvents = 'auto';
+    form.style.transition = 'all 0.3s ease-in-out';
+    form.scrollIntoView({ 
+        behavior: 'smooth', block: 'center' 
+    });
+    
+});
+
+validOK.addEventListener('click', function(e) {
+    form.style.display = 'none';
+    form.style.opacity = '0';
+    form.style.pointerEvents = 'none';
+    form.style.transition = 'all 0.3s ease-in-out';
+});
+
+
+// ################################
+// MANIPULATE FORM SUBMIT
 // ################################
